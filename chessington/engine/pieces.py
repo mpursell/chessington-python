bafb870948a39479e4f6f737b06d156b82b91c35
Mike <michael.pursell@gmail.com>
2022-02-03 15:57:38 +0000

    adding logic to pass unit tests

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -35,8 +35,57 @@ class Pawn(Piece):
     """
 
     def get_available_moves(self, board):
-        return []
 
+        current_square = board.find_piece(self)
+        row = current_square.row
+        column = current_square.col
+        moves = []
+
+        if self.player == Player.WHITE:
+            if row == 1:   
+                moves.append(Square((row + 2), column))
+            moves.append(Square((row + 1), column))
+
+            if row == 7:
+                return []
+
+            diagonal_left = Square(row +1, column - 1)
+            diagonal_right = Square(row + 1, column + 1)
+
+            if board.get_piece(diagonal_right) != None:
+            if board.get_piece(diagonal_right) == Pawn(Player.BLACK):    
+                moves.append(diagonal_right)
+
+            if board.get_piece(diagonal_left) != None:
+                moves.append(diagonal_left)
+
+            row_check = row + 1
+            square_check = Square(row_check, column)
+            if board.get_piece(square_check) != None:
+                return []
+
+        else:
+            if row == 6:
+                moves.append(Square((row - 2), column))
+            moves.append(Square((row - 1), column))
+
+            if row == 0:
+                return []
+
+            diagonal_left = Square(row - 1, column - 1)
+            diagonal_right = Square(row - 1, column + 1)
+
+            if board.get_piece(diagonal_right) != None:
+                moves.append(diagonal_right)
+
+            if board.get_piece(diagonal_left) != None:
+                moves.append(diagonal_left)
+
+            row_check = row - 1
+            square_check = Square(row_check, column)
+            if board.get_piece(square_check) != None:
+                return []
+        return moves
 
 class Knight(Piece):
     """
